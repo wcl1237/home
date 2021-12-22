68d0b694d3c8b77a9132c46ff4f42eba6774e7c2
王昌隆 <wangchanglong@corp.netease.com>
2021-12-22 17:50:40 +0800

    cdn

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
-import { defineConfig } from 'vite';
+import injectExterbals from 'vite-plugin-inject-externals'
+import {defineConfig} from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import hooks from '@midwayjs/vite-plugin-hooks';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [hooks(), reactRefresh()],
+    plugins: [
+        injectExterbals({
+            injectTo: '<!-- Custom placeholder for vite plugin inject externals -->',
+            modules: [
+                {
+                    name: 'react',
+                    global: 'React',
+                    path: 'https://unpkg.com/react@17/umd/react.production.min.js',
+                },
+                {
+                    name: 'react-dom',
+                    global: 'ReactDOM',
+                    path: 'https://unpkg.com/react-dom@17/umd/react-dom.production.min.js',
+                },
+            ],
+        }),hooks(),
+        reactRefresh(),],
 });
